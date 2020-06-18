7dd5ab44792ae670a241e620c4925141c35e9162
Luis Valero García <luisvaleroxx@gmail.com>
2020-06-18 21:06:01 +0200

    [refactor] [feat] Display Exercise

--- app/src/main/java/com/tfg/workoutagent/presentation/ui/exercises/trainer/viewmodels/ListExerciseViewModelFactory.kt
+++ app/src/main/java/com/tfg/workoutagent/presentation/ui/exercises/trainer/viewmodels/ListExerciseViewModelFactory.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.tfg.workoutagent.domain.exerciseUseCases.ListExercisesUseCase
 
-class ListExerciselViewModelFactory(private val listExerciseUseCase: ListExercisesUseCase):ViewModelProvider.Factory{
+class ListExerciseViewModelFactory(private val listExerciseUseCase: ListExercisesUseCase):ViewModelProvider.Factory{
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return modelClass.getConstructor(ListExercisesUseCase::class.java).newInstance(listExerciseUseCase)
     }
