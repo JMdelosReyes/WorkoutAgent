1ea3b08722c7a48bd765f7d6fd356891a6ff3796
Miguel <miguel.luque.97@gmail.com>
2020-07-20 18:30:36 +0200

    [feat] Filter recyclerView
    Este primer commit contiene lo necesario para hacer un filtro.
    Como ejemplo tenemos el filtro de los ejercicios.

--- app/src/main/java/com/tfg/workoutagent/presentation/ui/exercises/trainer/viewmodels/ListExerciseViewModel.kt
+++ app/src/main/java/com/tfg/workoutagent/presentation/ui/exercises/trainer/viewmodels/ListExerciseViewModel.kt
@@ -1,13 +1,16 @@
 package com.tfg.workoutagent.presentation.ui.exercises.trainer.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.tfg.workoutagent.domain.exerciseUseCases.ListExercisesUseCase
+import com.tfg.workoutagent.models.Exercise
 import com.tfg.workoutagent.vo.Resource
 import kotlinx.coroutines.Dispatchers
 
 class ListExerciseViewModel(listExerciseUseCase: ListExercisesUseCase): ViewModel() {
 
+    var filteredExerciseList = mutableListOf<Exercise>()
     val exerciseList = liveData(Dispatchers.IO) {
         emit(Resource.Loading())
         try{
