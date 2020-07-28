8bc5d5d994d48dc6e9217313438a07a79c636242
José Manuel de los Reyes <jmdelosreyesgarcia@gmail.com>
2020-07-28 09:08:32 +0200

    [refactor] Routines refactor

--- app/src/main/java/com/tfg/workoutagent/presentation/ui/routines/trainer/fragments/EditActivityEditRoutineFragment.kt
+++ app/src/main/java/com/tfg/workoutagent/presentation/ui/routines/trainer/fragments/EditActivityEditRoutineFragment.kt
@@ -1,6 +1,8 @@
 package com.tfg.workoutagent.presentation.ui.routines.trainer.fragments
 
 import android.app.Dialog
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,13 +15,19 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tfg.workoutagent.R
 import com.tfg.workoutagent.data.repositoriesImpl.ExerciseRepositoryImpl
 import com.tfg.workoutagent.data.repositoriesImpl.RoutineRepositoryImpl
 import com.tfg.workoutagent.databinding.FragmentEditActivityEditRoutineBinding
 import com.tfg.workoutagent.domain.routineUseCases.ManageRoutineUseCaseImpl
+import com.tfg.workoutagent.models.ActivitySet
+import com.tfg.workoutagent.presentation.ui.routines.trainer.adapters.CategoryListAdapter
+import com.tfg.workoutagent.presentation.ui.routines.trainer.adapters.ExerciseListAdapter
+import com.tfg.workoutagent.presentation.ui.routines.trainer.adapters.SetListAdapter
 import com.tfg.workoutagent.presentation.ui.routines.trainer.viewModels.EditRoutineViewModel
 import com.tfg.workoutagent.presentation.ui.routines.trainer.viewModels.EditRoutineViewModelFactory
+import com.tfg.workoutagent.vo.getAllCategories
 import kotlinx.android.synthetic.main.fragment_edit_activity_edit_routine.*
 
 class EditActivityEditRoutineFragment : DialogFragment() {
@@ -39,6 +47,12 @@ class EditActivityEditRoutineFragment : DialogFragment() {
             )
         ).get(EditRoutineViewModel::class.java)
     }
+
+    private lateinit var setListAdapter: SetListAdapter
+
+    private lateinit var categoryListAdapter: CategoryListAdapter
+
+    private lateinit var exerciseListAdapter: ExerciseListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,10 +83,47 @@ class EditActivityEditRoutineFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val darkMode = when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_NO -> false
+            else -> true
+        }
+        if (darkMode) {
+            nsv_edit_activity.setBackgroundResource(R.drawable.item_no_border_dark)
+            rl_edit_activity_plus.setBackgroundColor(context!!.resources.getColor(R.color.black_darkMode))
+            activity_note_input.setBackgroundColor(context!!.resources.getColor(R.color.black_darkMode))
+            activity_note_input_edit.setBackgroundColor(context!!.resources.getColor(R.color.black_darkMode))
+            repetitions_edit_activity_input.setTextColor(Color.WHITE)
+            weights_edit_activity_input.setTextColor(Color.WHITE)
+            activity_note_input_edit.setTextColor(Color.WHITE)
+        }
+        setupSetListRecycler()
+        setupCategoryListRecycler(darkMode)
+        setupExerciseListRecycler()
         setupToolbar()
-        setupSpinnerAdapter()
+        // setupSpinnerAdapter()
         observeErrors()
         observeDialogState()
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+        add_set_button.setOnClickListener {
+            val repetitions = repetitions_edit_activity_input.text.toString().toIntOrNull()
+            val weight = weights_edit_activity_input.text.toString().toDoubleOrNull()
+
+            val validSet = this.viewModel.addSet(repetitions, weight)
+            if (validSet == "") {
+                edit_activity_set_error_message.text = ""
+                edit_activity_set_error_message.visibility = View.GONE
+                this.setListAdapter.addElement(ActivitySet(repetitions!!, weight!!))
+                repetitions_edit_activity_input.text.clear()
+                weights_edit_activity_input.text.clear()
+                repetitions_edit_activity_input.requestFocus()
+            } else {
+                edit_activity_set_error_message.text = validSet
+                edit_activity_set_error_message.visibility = View.VISIBLE
+            }
+        }
     }
 
     private fun observeDialogState() {
@@ -87,7 +138,7 @@ class EditActivityEditRoutineFragment : DialogFragment() {
     }
 
     private fun observeErrors() {
-        viewModel.setsError.observe(viewLifecycleOwner, Observer {
+        /*viewModel.setsError.observe(viewLifecycleOwner, Observer {
             binding.activitySetInputEdit.error =
                 if (it != "") it else null
         })
@@ -100,7 +151,7 @@ class EditActivityEditRoutineFragment : DialogFragment() {
         viewModel.weightsError.observe(viewLifecycleOwner, Observer {
             binding.activityWeightsInputEdit.error =
                 if (it != "") it else null
-        })
+        })*/
 
         viewModel.noteError.observe(viewLifecycleOwner, Observer {
             binding.activityNoteInputEdit.error =
@@ -123,7 +174,7 @@ class EditActivityEditRoutineFragment : DialogFragment() {
             viewModel.clearActivityData()
             dismiss()
         }
-        toolbar.title = "Some Title"
+        toolbar.title = "Edit activity"
         toolbar.inflateMenu(R.menu.add_day_activity_dialog_menu)
         toolbar.setOnMenuItemClickListener {
             when (it.itemId) {
@@ -132,13 +183,14 @@ class EditActivityEditRoutineFragment : DialogFragment() {
                 }
                 else -> {
                     dismiss()
+                    this.viewModel.resetSelectedCategories()
                 }
             }
             true
         }
     }
 
-    private fun setupSpinnerAdapter() {
+    /*private fun setupSpinnerAdapter() {
         viewModel.exercises.observe(viewLifecycleOwner, Observer {
             it?.let {
                 val spinner: Spinner = activity_exercise_spinner
@@ -168,5 +220,86 @@ class EditActivityEditRoutineFragment : DialogFragment() {
             }
         })
         viewModel.getExercises()
+    }*/
+
+    private fun setupSetListRecycler() {
+        this.setListAdapter = SetListAdapter(
+            requireContext(),
+            { position: Int -> this.viewModel.removeSet(position) },
+            updateListener@{ repetitions, weight, position ->
+                val validSet = this.viewModel.checkSet(repetitions, weight)
+                if (validSet) {
+                    this.viewModel.updateSet(ActivitySet(repetitions!!, weight!!, true), position)
+                    return@updateListener ""
+                }
+                this.viewModel.updateSet(false, position)
+                return@updateListener "Invalid value"
+            })
+        recyclerView_activity_sets.layoutManager = LinearLayoutManager(this.requireContext())
+        recyclerView_activity_sets.adapter = setListAdapter
+        this.setListAdapter.setListData(this.viewModel.getNewSets())
+        this.setListAdapter.notifyDataSetChanged()
+    }
+
+    private fun setupCategoryListRecycler(darkMode: Boolean) {
+        this.categoryListAdapter =
+            CategoryListAdapter(this.requireContext()) { categoryName, categoryPosition, view ->
+                val currentCategories = this.viewModel.getSelectedCategories()
+                if (currentCategories.map { c -> c.name }.contains(categoryName)) {
+                    this.viewModel.removeSelectedCategory(categoryName, categoryPosition)
+                    if (darkMode) {
+                        view.setBackgroundResource(R.drawable.item_border_dark)
+                    } else {
+                        view.setBackgroundColor(Color.WHITE)
+                    }
+                } else {
+                    this.viewModel.addSelectedCategory(categoryName, categoryPosition)
+                    view.setBackgroundResource(R.drawable.item_border_primary_color)
+                }
+
+                val exercises = this.viewModel.updateAvailableExercises()
+                if (!exercises.contains(this.viewModel.getNewSelectedExercise())) {
+                    this.viewModel.updateNewSelectedExercise(null)
+                    this.exerciseListAdapter.setSelectedExercise(null)
+                }
+                this.exerciseListAdapter.setListData(exercises)
+            }
+
+        recyclerView_activity_categories.layoutManager =
+            LinearLayoutManager(this.requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerView_activity_categories.adapter = this.categoryListAdapter
+        this.categoryListAdapter.setListData(getAllCategories())
+    }
+
+    private fun setupExerciseListRecycler() {
+        this.exerciseListAdapter =
+            ExerciseListAdapter(this.requireContext()) updateExercise@{ exercise, _ ->
+                val selectedExercise = this.viewModel.getNewSelectedExercise()
+                if (selectedExercise == null) {
+                    this.viewModel.updateNewSelectedExercise(exercise)
+                    this.exerciseListAdapter.setSelectedExercise(exercise)
+                } else {
+                    if (selectedExercise.id == exercise.id) {
+                        this.viewModel.updateNewSelectedExercise(null)
+                        this.exerciseListAdapter.setSelectedExercise(null)
+                    } else {
+                        this.viewModel.updateNewSelectedExercise(exercise)
+                        this.exerciseListAdapter.setSelectedExercise(exercise)
+                    }
+                }
+            }
+        recyclerView_activity_exercises.layoutManager =
+            LinearLayoutManager(this.requireContext())
+        recyclerView_activity_exercises.adapter = this.exerciseListAdapter
+
+        viewModel.exercises.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                val exercises = this.viewModel.updateAvailableExercises()
+                this.exerciseListAdapter.setSelectedExercise(this.viewModel.getNewSelectedExercise())
+                this.exerciseListAdapter.setListData(exercises)
+            }
+        })
+
+        this.viewModel.getExercises()
     }
 }
