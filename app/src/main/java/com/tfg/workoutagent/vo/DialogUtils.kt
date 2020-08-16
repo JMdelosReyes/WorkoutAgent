b31538f2ff4f32370a017a396345f1012eeb31ea
Luis Valero García <luisvaleroxx@gmail.com>
2020-08-16 19:10:12 +0200

    [feat] Show description and note of activity as AlertDialog

--- app/src/main/java/com/tfg/workoutagent/vo/DialogUtils.kt
+++ app/src/main/java/com/tfg/workoutagent/vo/DialogUtils.kt
@@ -2,6 +2,7 @@ package com.tfg.workoutagent.vo
 
 import android.content.Context
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.tfg.workoutagent.R
 
 fun createAlertDialog(
     context: Context,
@@ -44,6 +45,19 @@ fun createAlertDialog(
         }
         .setNegativeButton(context.resources.getString(negativeText)) { dialog, _ ->
             negativeAction()
+            dialog.dismiss()
+        }
+        .show()
+}
+fun createAlertDialog(
+    context: Context,
+    title: String,
+    message: String
+) {
+    MaterialAlertDialogBuilder(context)
+        .setTitle(title)
+        .setMessage(message)
+        .setPositiveButton(context.getString(R.string.close)) { dialog, _ ->
             dialog.dismiss()
         }
         .show()
