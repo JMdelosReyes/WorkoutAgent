927fa1f64449ebcc5add556038cc364689b80fec
José Manuel de los Reyes <jmdelosreyesgarcia@gmail.com>
2020-08-17 17:38:42 +0200

    [fix] Achieved goal when swiped

--- app/src/main/java/com/tfg/workoutagent/presentation/ui/goals/customer/adapters/GoalsCustomerListAdapter.kt
+++ app/src/main/java/com/tfg/workoutagent/presentation/ui/goals/customer/adapters/GoalsCustomerListAdapter.kt
@@ -15,11 +15,18 @@ import kotlinx.android.synthetic.main.item_row_goal.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class GoalsCustomerListAdapter (private val context: Context, private val darkMode : Boolean):
-    RecyclerView.Adapter<GoalsCustomerListAdapter.GoalsCustomerListViewHolder> (){
+class GoalsCustomerListAdapter(private val context: Context, private val darkMode: Boolean) :
+    RecyclerView.Adapter<GoalsCustomerListAdapter.GoalsCustomerListViewHolder>() {
     var dataGoalsList = mutableListOf<Goal>()
-    fun setListData(data: MutableList<Goal>){
+    fun setListData(data: MutableList<Goal>) {
         dataGoalsList = data
+    }
+
+    fun updateElement(position: Int) {
+        val oldGoal = this.dataGoalsList[position]
+        oldGoal.isAchieved = true
+        this.dataGoalsList[position] = oldGoal
+        this.notifyItemChanged(position)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GoalsCustomerListViewHolder {
@@ -30,45 +37,47 @@ class GoalsCustomerListAdapter (private val context: Context, private val darkMo
     override fun getItemCount(): Int = if (dataGoalsList.size > 0) dataGoalsList.size else 0
 
     override fun onBindViewHolder(holder: GoalsCustomerListViewHolder, position: Int) {
-        val goal : Goal = dataGoalsList[position]
+        val goal: Goal = dataGoalsList[position]
         holder.itemView.setOnClickListener {
-           if(it.row_goal_description.isVisible){
-               it.row_goal_description.visibility = View.GONE
-           }else{
-               it.row_goal_description.visibility = View.VISIBLE
-           }
+            if (it.row_goal_description.isVisible) {
+                it.row_goal_description.visibility = View.GONE
+            } else {
+                it.row_goal_description.visibility = View.VISIBLE
+            }
         }
         holder.bindView(goal)
     }
 
-    inner class GoalsCustomerListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        fun bindView(goal: Goal){
-            if(darkMode) {
+    inner class GoalsCustomerListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindView(goal: Goal) {
+            if (darkMode) {
                 itemView.ll_all_goal.setBackgroundResource(R.drawable.item_border_dark)
-            }else{
+            } else {
                 itemView.ll_all_goal.setBackgroundResource(R.drawable.item_white_dark_border)
             }
-            if(goal.isAchieved){
-                Glide.with(context).load(R.drawable.ic_trophy_accomplished_48dp).into(itemView.circleImageViewGoal)
-            }else{
-                Glide.with(context).load(R.drawable.ic_trophy_not_accomplished_48dp).into(itemView.circleImageViewGoal)
+            if (goal.isAchieved) {
+                Glide.with(context).load(R.drawable.ic_trophy_accomplished_48dp)
+                    .into(itemView.circleImageViewGoal)
+            } else {
+                Glide.with(context).load(R.drawable.ic_trophy_not_accomplished_48dp)
+                    .into(itemView.circleImageViewGoal)
             }
             itemView.row_goal_aim.text = goal.aim
             itemView.row_goal_description.text = goal.description
             val currentTime = Calendar.getInstance().time
-            if(goal.startDate.before(currentTime) && goal.endDate.after(currentTime)){
-                val days_left = ((((goal.endDate.time - currentTime.time)/1000)/60)/60)/24
-                if(days_left.toInt() > 0){
+            if (goal.startDate.before(currentTime) && goal.endDate.after(currentTime)) {
+                val days_left = ((((goal.endDate.time - currentTime.time) / 1000) / 60) / 60) / 24
+                if (days_left.toInt() > 0) {
                     itemView.row_goal_deadline.text = "$days_left days left to go"
-                }else if (days_left.toInt() == 0){
+                } else if (days_left.toInt() == 0) {
                     itemView.row_goal_deadline.text = "Today is the last day"
                 }
-            }else if(goal.endDate.before(currentTime)){
+            } else if (goal.endDate.before(currentTime)) {
                 val pattern = "dd-MM-yyyy"
                 val simpleDateFormat = SimpleDateFormat(pattern)
                 val date: String = simpleDateFormat.format(goal.endDate)
                 itemView.row_goal_deadline.text = "This goal ended at $date"
-            }else if(goal.startDate.after(currentTime)){
+            } else if (goal.startDate.after(currentTime)) {
                 val pattern = "dd-MM-yyyy"
                 val simpleDateFormat = SimpleDateFormat(pattern)
                 val date: String = simpleDateFormat.format(goal.startDate)
